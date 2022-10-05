9dfee5212fb81cea98eb92d2194de4448f69a4d1
alothmanmj <alothman.maj@gmail.com>
2022-10-05 14:32:03 +0300

    wrote sort method header

--- Section54Sorter.java
+++ Section54Sorter.java
@@ -1,7 +1,14 @@
+import java.util.Arrays;
+
 public class Section54Sorter{
     
-    
+    static void sort(int[] arr){
+        System.out.println("To be impl");
+    }
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int [] arr = {1,0,2,7,3};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
